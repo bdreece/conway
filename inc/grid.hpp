02b89f4bcf0d6e73f6cbd7c0ee9e200893727842
Brian Reece <bdreece@mtu.edu>
2022-01-05 21:50:46 -0500

    Made sure I am able to show window

---
+++ inc/grid.hpp
@@ -0,0 +1,9 @@
+/**
+ * @file        grid.hpp
+ * @brief       Game of Life grid class
+ * @author      Brian Reece
+ * @version     0.1.0
+ * @date        01/05/2022
+ */
+
+class Grid {};
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
+#include <string>
 
-#include "renderer.hpp"
+#include "SDL.h"
+#include "util.hpp"
 
-int main() {
-  Renderer renderer(640, 480, 20, 20, false);
+int main(int argc, char **argv) {
+  if (argc < 5) {
+    return 1;
+  }
 
-  renderer.startRenderLoop(500);
+  int w = std::stoi(argv[1]);
+  int h = std::stoi(argv[2]);
+  int m = std::stoi(argv[3]);
+  int n = std::stoi(argv[4]);
 
+  ASSERT(SDL_Init(SDL_INIT_VIDEO) <= 0);
+  SDL_Window *window =
+      SDL_CreateWindow("conway", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                       w, h, 0);
+  ASSERT(window);
+
+  SDL_Delay(5000);
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
   return 0;
-}
+};
