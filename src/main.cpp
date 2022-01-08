83125c38dc7901d0e1443d0201ff98f25ab1399a
Brian Reece <bdreece@mtu.edu>
2022-01-07 19:39:14 -0500

    Tried to add OpenMP, realized my mistakes

--- src/main.cpp
+++ src/main.cpp
@@ -82,7 +82,7 @@ int main(int argc, char **argv) {
 
     // Update grid with cells
     for (int i = 0; i < total; i++) {
-      grid.updateCell(i, (cells[i] == 1) ? ALIVE : DEAD);
+      grid.updateCell(i, cells[i] == 1 ? ALIVE : DEAD);
     }
 
     // Flush grid
